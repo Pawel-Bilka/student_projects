9432e623f9774a4d997cc51d52b66ae68e41de9e
Pawel-Bilka <pawel.bilka@e-zstih.edu.pl>
2024-01-18 13:53:24 +0000

    studentmachine send

---
+++ cpp/cwiczenie_if/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+int main(){
+int x;
+
+cout<<"podaj wiek"<<endl;
+cin>>x;
+
+if(x>=18) 
+cout<<"Jestes pelnoletni"<<endl;
+
+
+
+
+if(x<18)
+cout<<"jestes niepelnoletni"<<endl;
+
+
+
+
+	return 0;
+}
