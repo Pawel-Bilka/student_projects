2f942605a6d9e3e26115d308cca779f59f72412d
Pawel-Bilka <pawel.bilka@e-zstih.edu.pl>
2024-04-11 13:06:24 +0000

    studentmachine send

---
+++ python/cwiczenie1/main.py
@@ -0,0 +1,12 @@
+print ("witaj swiecie")
+for z in range(1,10):
+	print(z)
+y=float(input("podaj wartosc y"))
+x=float(input("podaj wartosc x"))
+if x>10:
+	print("wiekszy od 10")
+elif x==10 and y==40:
+	print("paobao")
+else:
+	print("mniejszy od 10")
+print (x+y)
